ea97982caca605f365ba16c8f8a1d5a0c4b778b9
jiboo01 <130724366+jiboo01@users.noreply.github.com>
2023-07-03 18:40:06 +0900

    code explanation

--- 3. Price Prediction (ML Model)/오차율 비율로.py
+++ 3. Price Prediction (ML Model)/오차율 비율로.py
@@ -5,71 +5,59 @@ import pymysql
 from sqlalchemy import create_engine
 import time
 from sklearn.preprocessing import StandardScaler, normalize, RobustScaler, MinMaxScaler, FunctionTransformer
+from scipy.stats import boxcox
 from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import scipy.cluster.hierarchy as hc
+from sklearn.cluster import AgglomerativeClustering
+from haversine import haversine, Unit
 
-# Measure time taken to import table
 start_time = time.time()
-
-# Connect to MySQL database
 con = pymysql.connect(host='localhost', user='jiwoo', password='1234', db='TESTDB', charset='utf8', autocommit=True, cursorclass=pymysql.cursors.DictCursor)
 cur = con.cursor()
-
-# Data for ML are stored in table 'off_act_price'
-# Extracting data by administrative districts; in this case 대전광역시(Daejeon)
-
-# << Columns explained >>
-# 시도 : Administrative District in Korea
-# official_price : Appraised Price of a certain unit within the apartment(공시지가)
-# 경도 : longitude
-# 위도 : latitude
-# closest_sub : Direct distance to closest subway st. from apartment
-# closest_high : Direct distance to closest high school st. from apartment
-# school_1km : Number of elementary/middle/high schools within a 1km radius from apartment
-# area : Area(pyeong) of a certain unit (pyeong = square meter/3.3)
-# floor : Floor that a certain unit is located
-# actual : Transaction Price of a certain unit within the apartment(실거래가)
-sql = "SELECT pk, 시도, official_price, 경도, 위도, closest_sub, closest_high, school_1km, area, floor, actual FROM off_act_price WHERE 시도='대전광역시'"
+sql = "SELECT pk, 시도, official_price, 경도, 위도, closest_sub, closest_high, school_1km, area, floor, actual FROM off_act_price WHERE 시도='강원도'"
 cur.execute(sql)
 rows = cur.fetchall()
 con.close()
 
-# Turn table into a dataframe
-daejeon = pd.DataFrame(rows)
+gangwon = pd.DataFrame(rows)
 end_time = time.time()
 taketime = str(end_time - start_time)
-print("대전광역시 import time (s) : " + taketime)
+print("강원도 import 실행 소요시간 (단위 초) : " + taketime)
 
-# Sort data that have transaction price 
-real_trainInfo = daejeon.dropna(subset='actual')
+real_trainInfo = gangwon.dropna(subset='actual')
 
-# Sort data that lack of transaction price
-needPredict = daejeon[daejeon['actual'].isnull()==True]
+needPredict = gangwon[gangwon['actual'].isnull()==True]
 
-# Delete target variable to make train dataset
+
+
 X = real_trainInfo.drop('actual', axis=1)
 y = real_trainInfo['actual']
 
-# Split train/valid/test set to 6:2:2
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 x_train, x_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.25, random_state=1)
 
-# Extract 'pk' column to use as key value
 train_pk = x_train['pk']
 valid_pk = x_valid['pk']
 test_pk = X_test['pk']
 predict_pk = needPredict['pk']
 
-# Change negative values to positive for normalization
 train_data = x_train.join(y_train)
 train_data['floor'] = train_data['floor']+2
-train_data['school_1km'] = train_data['school_1km']+2
 needPredict['floor'] = needPredict['floor']+2
+#train_data['1km내sub수+'] = train_data['1km내sub수']+2
+train_data['school_1km'] = train_data['school_1km']+2
 needPredict['school_1km'] = needPredict['school_1km']+2
 
-# Normalization using log, 
+from scipy.stats import boxcox
+#train_data['area'] = train_data['area'].apply(lambda x: boxcox(x)[0])
 train_data[['school_1km', 'area']] = np.sqrt(train_data[['school_1km', 'area']])
 train_data[['actual','floor', 'official_price', 'closest_sub', 'closest_high']] = np.log(train_data[['actual','floor', 'official_price', 'closest_sub', 'closest_high']])
+#needPredict['area'] = needPredict['area'].apply(lambda x: boxcox(x)[0])
 needPredict[['school_1km', 'area']] = np.sqrt(needPredict[['school_1km', 'area']])
 needPredict[['floor', 'official_price', 'closest_sub', 'closest_high']] = np.log(needPredict[['floor', 'official_price', 'closest_sub', 'closest_high']])
 
@@ -80,8 +68,10 @@ x_train, y_train = train_data.drop(['actual', 'pk', '시도'], axis=1), train_da
 valid_data = x_valid.join(y_valid)
 
 valid_data['floor'] = valid_data['floor']+2
+#valid_data['1km내sub수+'] = valid_data['1km내sub수']+2
 valid_data['school_1km'] = valid_data['school_1km']+2
 
+#valid_data['area'] = valid_data['area'].apply(lambda x: boxcox(x)[0])
 valid_data[['school_1km', 'area']] = np.sqrt(valid_data[['school_1km', 'area']])
 valid_data[['actual','floor', 'official_price', 'closest_sub', 'closest_high']] = np.log(valid_data[['actual','floor', 'official_price', 'closest_sub', 'closest_high']])
 
@@ -90,13 +80,15 @@ x_valid, y_valid = valid_data.drop(['actual', 'pk', '시도'], axis=1), valid_da
 test_data = X_test.join(y_test)
 
 test_data['floor'] = test_data['floor']+2
+#valid_data['1km내sub수+'] = valid_data['1km내sub수']+2
 test_data['school_1km'] = test_data['school_1km']+2
 
+#valid_data['area'] = valid_data['area'].apply(lambda x: boxcox(x)[0])
 test_data[['school_1km', 'area']] = np.sqrt(test_data[['school_1km', 'area']])
-test_data[['actual_price','floor', 'official_price', 'closest_sub', 'closest_high']] = np.log(test_data[['actual','floor', 'official_price', 'closest_sub', 'closest_high']])
+test_data[['actual','floor', 'official_price', 'closest_sub', 'closest_high']] = np.log(test_data[['actual','floor', 'official_price', 'closest_sub', 'closest_high']])
 
 y_test_actual_price = test_data['actual']
-X_test, y_test = test_data.drop(['actual', 'actual_price', 'pk', '시도'], axis=1), test_data['actual_price']
+X_test, y_test = test_data.drop(['actual', 'pk', '시도'], axis=1), test_data['actual']
 
 scaler = MinMaxScaler()
 
@@ -155,66 +147,56 @@ final_r2_tree = r2_score(y_test, final_pred_tree)
 print('Test Set RMSE: ', final_rmse_tree)
 print('Test Set R2: ', final_r2_tree)
 
-needPredict_s = scaler.transform(realX)
-finalPred = tree.predict(needPredict_s)
-needPred_df = pd.DataFrame(finalPred, columns=['예측가격'])
-needPred_df['pk'] = predict_pk.values
-needPred_df['predicted_price'] = np.exp(needPred_df['예측가격'])
-needPred_df.drop('예측가격', axis=1, inplace=True)
-print(' ')
-print('<< null 예측값 >>')
-print(needPred_df)
+#needPredict_s = scaler.transform(realX)
+#finalPred = tree.predict(needPredict_s)
+#needPred_df = pd.DataFrame(finalPred, columns=['예측가격'])
+#needPred_df['pk'] = predict_pk.values
+#needPred_df['predicted_price'] = np.exp(needPred_df['예측가격'])
+#needPred_df.drop('예측가격', axis=1, inplace=True)
+#print(' ')
+#print('<< null 예측값 >>')
+#print(needPred_df)
 
 # 예측값
 pred_tr = pd.DataFrame(final_pred_tree, columns=['예측가격'])
 pred_tr['pk'] = test_pk.values
 pred_tr['predicted_price'] = np.exp(pred_tr['예측가격'])
-pred_tr['actual_price'] = y_test_actual_price.values
+pred_tr['actual_price_unscaled'] = y_test_actual_price.values
+pred_tr['actual_price'] = np.exp(pred_tr['actual_price_unscaled'])
+pred_tr = pred_tr.drop('actual_price_unscaled', axis=1)
 pred_tr['predicted_price'] = pred_tr['predicted_price'].astype(float)
-
+# 결측치가 있는 것 같지는 않고 nan 이런 식으로 숫자가 아닌 데이터가 있는 듯. 확인해보기 바람
 pred_tr.drop('예측가격', axis=1, inplace=True)
-
+#pred_tr['predicted_price'] = pred_tr['predicted_price'].apply(lambda x: '{:.0f}'.format(x))
+#jisang['codes'] = jisang['호str'].apply(lambda x: int(x) if x.isnumeric() else 0)
 
 pred_tr['error'] = (pred_tr['actual_price'] - pred_tr['predicted_price']) / pred_tr['actual_price'] * 100
 print(' ')
 print(pred_tr.describe())
-pred_tr['error'].hist(bins=50)
-plt.rc("axes", unicode_minus=False)
-plt.rc('font', family='NanumGothic')
-plt.title('대전광역시 의사결정나무 오차율 분포')
+
+error_values = pred_tr['error']
+
+# Define the custom bin ranges
+bin_ranges = [
+    (-500, -50), (-50, -40), (-40, -30), (-30, -20), (-20, -10), (-10, 0),
+    (0,10), (10,20), (20,30), (30,40), (40,50), (50, 500)
+    # Add more custom ranges as needed
+]
+
+# Assign bins to data
+binned_data = pd.cut(error_values, bins=[range[0] for range in bin_ranges] + [bin_ranges[-1][1]], labels=[i for i in range(len(bin_ranges))])
+
+# Calculate proportions
+proportions = binned_data.value_counts(normalize=True).sort_index()
+
+# Plot the histogram
+plt.bar([f'{range[0]}% to {range[1]}%' for range in bin_ranges], proportions.values)
+plt.xlabel('Error Range')
+plt.ylabel('Proportion')
+plt.title('Proportion of Errors in Gangwon')
+plt.xticks(rotation=45)
+plt.grid(True, axis='y', linestyle='--')
+
+# Show the plot
 plt.show()
-
-test_df = pred_tr[['pk', 'predicted_price']]
-total_df = pd.concat([needPred_df, test_df])
-
-
-final_tr = pred_tr[['predicted_price', 'actual_price']]
-
-plt.rc('font', family='NanumGothic')
-final_tr.iloc[1700:1800, :].plot(figsize=(30,7))
-plt.show()
-
-
-engine = create_engine("mysql+pymysql://jiwoo:1234@localhost:3306/TESTDB?charset=utf8mb4")
-conn = engine.connect()
-table_name = 'predicted_price'
-start_time = time.time()
-total_df.to_sql(name=table_name, con=engine, if_exists='append', index=False)
-end_time = time.time()
-taketime = str(end_time - start_time)
-print(table_name+" 스크립트 실행 소요시간 (단위 초) : " + taketime)
-print(' ')
-print('<< test set & null 예측값 >>')
-print(total_df)
-    
-
-#pred_tr['predicted_price'] = np.exp(pred_tr['예측가격'])
-#table_tr = pd.concat([needPredict, pred_tr], axis=1)
-#final_table = pd.concat([pk_predict, table_tr], axis=1)
-
-
-pd.set_option('display.max_columns', None)
-print(' ')
-print('======== test set 실제값/예측값 비교 =========')
-print(final_tr)
 
